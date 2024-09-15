e678ab20ad3a5ffcf8cdaead42dc3a8095d9cfbf
Mosya-CX <1906848521@qq.com>
2024-09-15 14:50:33 +0800

    1

---
+++ Assets/Scripts/Missile/BaseMissile.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "创建新的投掷物", fileName = "newMissle")]
+public class BaseMissile : ScriptableObject
+{
+    public float attackSpace;
+    public float damage;
+    public float durationTime;
+    public float speed;
+    public float size;
+    public Sprite image;
+    public MissileEffect effect;
+}
